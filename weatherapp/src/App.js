006d79b9c1db556bceaa1104c47e7a6b895e620f
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-06-02 20:10:52 -0500

    testing

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const apikey = process.env.REACT_APP_API_KEY;
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      data: [],
+     }
+  }
+
+async componentDidMount () {
+  try {
+  const weather = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=Brooklyn&appid=${apikey}`)
+  this.setState({data: weather.data.list});
+  console.log(this.state.data);
+  } catch(e) {
+    console.log('error', e)
+  }
+  
+}
+      render() { 
+        
+        return ( 
+          <div>
+        {this.state.data.map(forecast => (
+         <div>
+             <h1> Max {forecast.main.temp_min} </h1>
+             <h1> Min {forecast.main.temp_max} </h1>
+             <p> {forecast.weather[0].description} </p>
+         </div>
+         ))}
+         </div>
+          );
+        }
+      }
+
+ 
 export default App;
